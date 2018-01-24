8f2741d8f4c89d94a810abc9bcc52e10970d0f31
Angelika Jarosz <angelikajarosz@gmail.com>
2018-01-24 16:42:10 -0500

    proxy to petfinder api

--- backend/catcall/urls.py
+++ backend/catcall/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from .views import petfinder_view
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^api/(?P<path>.*)$', petfinder_view)]
+
---
+++ backend/catcall/views.py
@@ -0,0 +1,9 @@
+from settings import PETFINDER_API_KEY
+from proxy.views import proxy_view
+
+def petfinder_view(request, path):
+    requests_args = {'params': {'key': PETFINDER_API_KEY, 'format':'json', 'animal': 'cat'}}
+    remoteurl = 'http://api.petfinder.com/' + path
+    return proxy_view(request, remoteurl, requests_args)
+
+
