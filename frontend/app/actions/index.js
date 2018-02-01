f7a2b970058956ac090d625376b2d75a26ec054d
Angelika Jarosz <angelikajarosz@gmail.com>
2018-02-01 16:07:00 -0500

    actions and reducer for random cat api call

---
+++ frontend/app/actions/index.js
@@ -0,0 +1,49 @@
+export const RANDOM_CAT_REQUEST = 'RANDOM_CAT_REQUEST'
+export const RANDOM_CAT_SUCCESS = 'RANDOM_CAT_SUCCESS'
+export const RANDOM_CAT_FAILURE = 'RANDOM_CAT_FAILURE'
+export const RESET_ERROR = 'RESET_ERROR'
+
+export function randomCatRequest() {
+  return {
+    type: RANDOM_CAT_REQUEST,
+    RANDOM_CAT_REQUEST
+  }
+}
+
+export function randomCatSuccess(json) {
+  return {
+    type: RANDOM_CAT_SUCCESS,
+    randomCat: json
+  }
+}
+
+export function randomCatFailure(error) {
+  return {
+    type: RANDOM_CAT_FAILURE,
+    error: error.message || 'Something went wrong'
+  }
+}
+
+export function resetError() {
+  return {
+    type: RESET_ERROR
+  }
+}
+
+function handleErrors(response) {
+  if(!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
+}
+
+export function fetch_random_cat() {
+  return function (dispatch) {
+    dispatch(randomCatRequest)
+    return fetch('/api/pet.getRandom?&output=full')
+      .then(handleErrors)
+      .then(response =>
+            response.json().then(json => dispatch(randomCatSuccess(json))))
+      .catch(error => dispatch(randomCatFailure(error)))
+  }
+}
