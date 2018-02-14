e777380f8a3ba6c0f7b47952aca48d78db32acc1
Angelika Jarosz <angelikajarosz@gmail.com>
2018-02-14 17:02:25 -0500

    09 redux architecture for api call (#11)
    * update package.json
    
    * actions and reducer for random cat api call
    
    * update package.json, create store, store receives random cat json object
    
    * implement redux api middleware pattern for api calls

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -2,13 +2,30 @@ require('bootstrap/dist/css/bootstrap.min.css');
 
 import React from 'react'
 import { render } from 'react-dom'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { apiMiddleware } from 'redux-api-middleware'
+import { Provider } from 'react-redux'
 import App from './app'
+import randomCat from './reducers/catReducer'
+import { fetchRandomCat } from './actions/catActions'
 
+
+const store = createStore(
+  randomCat,
+  applyMiddleware(
+    apiMiddleware,
+    thunkMiddleware
+  )
+)
+
+store.dispatch(fetchRandomCat())
+  .then(() => console.log(store.getState()))
 
 render(
-    <div>
+    <Provider store={store}>
       <App />
-    </div>,
+    </Provider>,
   document.getElementById('react-app'))
 
 module.hot.accept()
