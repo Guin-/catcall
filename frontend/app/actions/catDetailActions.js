e82e820b2afc6c59cc0ba8bd67b12fb2008e5ca5
Angelika Jarosz <angelikajarosz@gmail.com>
2018-03-25 16:51:43 -0400

    clean data function creates and returns new object

--- frontend/app/actions/catDetailActions.js
+++ frontend/app/actions/catDetailActions.js
@@ -6,23 +6,25 @@ export const SUCCESS = 'CAT_DETAIL_SUCCESS'
 export const FAILURE = 'CAT_DETAIL_FAILURE'
 
 export function cleanCatData(pet) {
-  let result = _.forEach(pet, function(value, parentKey) {
-                  _.forEach(value, function(value, key) {
-                    if (_.isArray(value)) {
-                        pet[parentKey] = _.map(value, '$t')
-                    }
-                    else if (key==='$t') {
-                      pet[parentKey] = value
-                    }
-                    else {
-                       pet[key] = value['$t'] || value
-                       delete pet[parentKey]
-                    }
-                  })
-                })
-  return result
+  let cleanData = {}
+  _.forEach(pet, function(value, parentKey) {
+    _.forEach(value, function(value, key) {
+      if (_.isArray(value)) {
+          pet[parentKey] = _.map(value, '$t')
+          cleanData[parentKey] = pet[parentKey]
+      }
+      else if (key==='$t') {
+        pet[parentKey] = value
+        cleanData[parentKey] = pet[parentKey]
+      }
+      else {
+         pet[key] = value['$t'] || value
+         cleanData[key] = pet[key]
+      }
+    })
+  })
+  return cleanData
 }
-
 
 export function fetchCatDetail(id) {
   return {
