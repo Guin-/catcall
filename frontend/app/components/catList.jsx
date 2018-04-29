a989ab1282d92e7bb06ee58f2bb964b4ac80d07b
Angelika Jarosz <angelikajarosz@gmail.com>
2018-04-29 15:34:03 -0400

    cat cards lazy load, on large devices data is grouped by 3 and a group is wrapped with a bootstrap row

--- frontend/app/components/catList.jsx
+++ frontend/app/components/catList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CatCard from './catCard'
 import { Grid, Row } from 'react-bootstrap'
+import Infinite from 'react-infinite'
+import Media from 'react-media'
 
 class CatList extends React.Component {
   constructor(props) {
@@ -9,16 +11,35 @@ class CatList extends React.Component {
 
   render() {
     const cats = this.props.data
-    const catItems = cats.map((cat, id) => {
-      return(
-        <CatCard cat={cat} key={cat.id} />
-      )
+    const groupSize=3
+    const mobileRows = cats.map((cat, id) => {
+        return(
+            <CatCard cat={cat} key={cat.id} />
+        )
     })
+    const rows = mobileRows.reduce((row, element, index)=>{
+        index % groupSize === 0 && row.push([]);
+        row[row.length - 1].push(element);
+        return row
+    }, []).map((rowContent, index) => {
+      return <div className="row" key={index}>{rowContent}</div>
+    })
+
     return (
       <Grid>
-        <Row>
-          {catItems}
-        </Row>
+        <Media query="(max-width: 1024px)">
+        {matches =>
+            matches ? (
+              <Infinite elementHeight={400} useWindowAsScrollContainer>
+                {mobileRows}
+              </Infinite>
+            ) : (
+              <Infinite elementHeight={400} useWindowAsScrollContainer>
+                {rows}
+              </Infinite>
+            )
+          }
+        </Media>
       </Grid>
     )
   }
