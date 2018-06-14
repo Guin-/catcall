c586751081029c1efcf65fe3febd49a90a35a427
Angelika Jarosz <angelikajarosz@gmail.com>
2018-06-14 13:39:15 -0400

    about page (#63)

--- frontend/app/components/about.jsx
+++ frontend/app/components/about.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-bootstrap'
+import github from '../../static/github.png'
+import mail from '../../static/mail.png'
+import location from '../../static/location.png'
 
-const About = () => (
-  <div className='about'>
-    <p> I am the about component </p>
-  </div>
-)
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Grid className="view about">
+        <Row>
+          <Col lg={8} lgOffset={2} md={8} mdOffset={2} xs={10} xsOffset={1} className="text-center">
+            <div>
+              <h1>About</h1>
+              <div className="about-copy">
+                <p>
+                  Cat Call is a web app that helps users search for adoptable cats by their location.
+                  It provides important information regarding each cat and the contact information for the shelter so that you may move forward with the adoption process.
+                  All data is provided by the Petfinder API.
+                  It was built with Django, React, Redux, and React Bootstrap.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h1>Contact</h1>
+                <div className="about-copy">
+                  <div className='icon-image-container'>
+                    <a href="https://github.com/Guin-/"><img className="icon-image inverted"src={github}/></a>
+                    <p>@Guin-</p>
+                  </div>
+                  <div className='icon-image-container'>
+                    <a href="mailto:angelikajarosz@gmail.com"><img className="icon-image inverted" src={mail}/></a>
+                    <p>angelikajarosz@gmail.com</p>
+                  </div>
+                  <div className='icon-image-container'>
+                    <img className="icon-image inverted" src={location}/>
+                    <p>Brooklyn, NY</p>
+                  </div>
+                </div>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
 
 export default About;
