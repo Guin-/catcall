d3425dfb6dedf33e3b52c1dab3a0c5e363451691
Angelika Jarosz <angelikajarosz@gmail.com>
2019-09-19 14:55:07 -0400

    add environment variables, helper function to get access token for successful response from petfinder v2 api

--- backend/catcall/settings/base.py
+++ backend/catcall/settings/base.py
@@ -30,6 +30,7 @@ def get_env_variable(var_name):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable('SECRET_KEY')
 PETFINDER_API_KEY = get_env_variable('PETFINDER_API_KEY')
+PETFINDER_SECRET = get_env_variable('PETFINDER_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/catcall/views.py
+++ backend/catcall/views.py
@@ -1,9 +1,23 @@
-from catcall.settings.base import PETFINDER_API_KEY
+from catcall.settings.base import PETFINDER_API_KEY, PETFINDER_SECRET
 from proxy.views import proxy_view
+import requests
+
+def authenticate():
+    url = 'http://api.petfinder.com/v2/oauth2/token'
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': PETFINDER_API_KEY,
+        'client_secret': PETFINDER_SECRET
+    }
+
+    r = requests.post(url, data=data)
+    return r.json()['access_token']
 
 def petfinder_view(request, path):
-    requests_args = {'params': {'key': PETFINDER_API_KEY, 'format':'json', 'animal': 'cat', 'count': 30 }}
-    remoteurl = 'http://api.petfinder.com/' + path
+    requests_args = { 'headers': {'Authorization': 'Bearer ' + authenticate()},
+                      'params': { 'type': 'cat', 'limit': 30 }
+                     }
+    remoteurl = 'http://api.petfinder.com/v2/' + path
     return proxy_view(request, remoteurl, requests_args)
 
 
