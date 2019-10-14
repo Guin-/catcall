3cb965b5200b5127ef0ba2c1d5d981ba49d9829e
Angelika Jarosz <angelikajarosz@gmail.com>
2019-10-14 14:23:33 -0400

    Detail view (#103)
    * use medium photo instead of small thumbnail
    
    * remove old catIntro component
    
    * extract data for and conditionally render detail health attributes, enviornment, and tags. Shelter contact data. Remove bootstrap grid.
    
    * code clean up, remove unused import

--- frontend/app/components/catDetail.jsx
+++ frontend/app/components/catDetail.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
 
 class CatDetail extends React.Component {
   constructor(props) {
@@ -8,13 +7,51 @@ class CatDetail extends React.Component {
 
   render() {
     const { cat } = this.props
+    const { attributes, environment, tags } = this.props.cat
+    const health = Object.keys(attributes).filter(key => attributes[key])
+    const okWith = Object.keys(environment).filter(key => environment[key])
+    const notOkWith = Object.keys(environment).filter(key => !environment[key] == null  && !environment[key])
     return (
-      <Col xs={12} xsOffset={0} smOffset={1} mdOffset={1} lg={6} lgOffset={0}>
-        <div className="cat-detail-container">
-          <h1> Meet { cat.name }</h1>
-          <p> { cat.description ? cat.description : "Please use the given contact information to find out more about me!" }</p>
+        <div className="div3 area">
+        <div className="div4 about-inner">
+          <div>
+            {
+              (health.length > 0) &&
+              <div>
+                 <h3> Health & Behavior </h3>
+                 <p> {health} </p>
+              </div>
+            }
+            {
+              (okWith.length > 0) &&
+              <div>
+                <h3> Ok With </h3>
+                <p>{okWith}</p>
+              </div>
+            }
+            {
+              (notOkWith.length > 0) &&
+              <div>
+                <h3> Prefers Home Without </h3>
+                <p>{notOkWith}</p>
+              </div>
+            }
+            {
+              (tags.length > 0) &&
+                <div>
+                  <h3> Characteristics </h3>
+                  <p> {tags.map(tag => tag)}</p>
+                </div>
+              }
+          </div>
         </div>
-      </Col>
+        <div className="div5 about-inner">
+          <div>
+            <h3> Meet {cat.name} </h3>
+            <p> { cat.description ? cat.description : "Please use the given contact information to find out more about me!" }</p>
+          </div>
+        </div>
+        </div>
     )
   }
 }
--- frontend/app/components/shelterDetail.jsx
+++ frontend/app/components/shelterDetail.jsx
@@ -12,27 +12,16 @@ class ShelterDetail extends React.Component {
   render() {
     const { cat } = this.props
     return (
-      <Col xs={12} xsOffset={0} smOffset={1} mdOffset={1} lg={6} lgOffset={0}>
-        <div className="cat-detail-container">
-          <h1 className="cat-detail-header">Contact</h1>
-            <div className="icon-item">
-              <img className="icon-image" src={email} alt="email"/>
-              <span className="icon-item-data">{cat.contact ? cat.contact['email'] : 'N/A'}</span>
-            </div>
-            <div className="icon-item">
-              <img className="icon-image" src={phone} alt="phone"/>
-              <span className="icon-item-data">{cat.contact['phone'] || 'N/A'}</span>
-            </div>
-            <div className="icon-item">
-              <img className="icon-image" src={address} alt="address"/>
-              <div className="icon-item-data">
-                <p className="address">{cat.contact.address['address1'] || 'N/A' && cat.contact.address['address2']}</p>
-                <p className="address">{cat.contact.address['city']} {cat.contact.address['state']} {cat.contact.address['postcode']}
-                </p>
-              </div>
-            </div>
+        <div className="div2 area">
+          <div className="contact-container">
+            <h2> Contact </h2>
+            <p>{cat.contact.phone}</p>
+            <p>{cat.contact.email}</p>
+            <p>{cat.contact.address.address1}</p>
+            <p>{cat.contact.address.address2}</p>
+            <p>{cat.contact.address.city} {cat.contact.address.state} {cat.contact.address.postcode}</p>
+          </div>
         </div>
-      </Col>
     )
   }
 }
