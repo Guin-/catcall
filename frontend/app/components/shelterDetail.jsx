b880bbb85160c5b53bd70e7244452bbcbcd16100
Angelika Jarosz <angelikajarosz@gmail.com>
2019-10-25 11:41:59 -0400

    resolves #111

--- frontend/app/components/shelterDetail.jsx
+++ frontend/app/components/shelterDetail.jsx
@@ -12,12 +12,22 @@ class ShelterDetail extends React.Component {
         <Col lg={5} md={6}>
           <div className="detail-section">
             <div className="contact-container">
-              <h2> Adopt {cat.name}</h2>
-              <p>{cat.contact.phone}</p>
-              <p>{cat.contact.email}</p>
-              <p>{cat.contact.address.address1}</p>
-              <p>{cat.contact.address.address2}</p>
+              <h2 className="contact-heading"> Adopt {cat.name}</h2>
+              <div className="contact-details">
+              { cat.contact.phone ?
+              <p>{cat.contact.phone}</p> : null
+              }
+              { cat.contact.email ?
+              <p>{cat.contact.email}</p> : null
+              }
+              {cat.contact.address.address1 ?
+              <p>{cat.contact.address.address1}</p> : null
+              }
+              { cat.contact.address.address2 ?
+              <p>{cat.contact.address.address2}</p> : null
+              }
               <p>{cat.contact.address.city} {cat.contact.address.state} {cat.contact.address.postcode}</p>
+              </div>
             </div>
           </div>
         </Col>
