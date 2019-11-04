71f5fce18f08ad4dae95c7d78ea9301369bed707
Angelika Jarosz <angelikajarosz@gmail.com>
2019-11-04 11:12:41 -0500

    092 loading list offset data bugs (#119)
    * loading component does not take up full vh, added full vh to list view so background color fills full height
    
    * adds cat list clear on mount of list container, removes redundant call to set isInfiniteLoading state, changes offset for desktop
    
    * code cleanup, remove unnecessary imports

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,5 +1,4 @@
 require('bootstrap/dist/css/bootstrap.min.css');
-require('bootstrap/dist/fonts/glyphicons-halflings-regular.woff');
 
 import React from 'react'
 import "babel-polyfill";
@@ -10,13 +9,7 @@ import { apiMiddleware } from 'redux-api-middleware'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './app'
-import ScrollToTop from '../app/components/scrollToTop'
-import { fetchBreeds } from './actions/breedActions'
-import { fetchCatDetail } from './actions/catDetailActions'
-import { fetchCatList } from './actions/catListActions'
-import { persistZip } from './actions/persistZip'
 import rootReducer from './reducers/index'
-import glyphicons from '../static/glyphicons-halfings-regular.woff'
 import '../app/styles/main.css'
 
 const store = createStore(
@@ -31,9 +24,7 @@ const store = createStore(
 render(
     <Provider store={store}>
       <Router>
-        <ScrollToTop>
           <App />
-        </ScrollToTop>
       </Router>
     </Provider>,
   document.getElementById('react-app'))
